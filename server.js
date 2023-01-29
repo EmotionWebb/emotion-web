a2bfca8ce6191c939b827d415695e4a80bb60a3c
Ganesh <ganesh.birajdar.1930@gmail.com>
2023-01-29 19:15:27 +0530

    fixed: env file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require("express");
-var app = express();
-var path = require("path");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
 // set the view engine to ejs
 // app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/public", express.static("public"));
 // app.use(express.static(path.join(__dirname, "public")));
@@ -14,5 +18,8 @@ app.get("/", function (req, res) {
   res.render("pages/index");
 });
 
-app.listen(8080);
-console.log("Server is listening on port 8080");
+// app.listen(8080);
+// console.log("Server is listening on port 8080");
+app.listen(process.env.PORT || 8080, () =>
+  console.log("Server is running at 4000")
+);
